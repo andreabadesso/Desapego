77dab595df85b3de1044ac1a29093a428ee34413
André Abadesso <andre.abadesso@gmail.com>
2014-03-13 14:50:10 -0300

    Iniciei curtir

--- Desapegos/models.py
+++ Desapegos/models.py
@@ -14,6 +14,7 @@ class Desapego(models.Model):
 	latitude = models.CharField(max_length=150, default="-", null=True)
 	longitude = models.CharField(max_length=150, default="-", null=True)
 	comentarios = models.ManyToManyField(Comentario, null=True, related_name="desapego")
+	curtidas = models.ManyToManyField(Usuario, null=True, blank=True, related_name="curtiu")
 	created_at = models.DateTimeField(auto_now_add=True)
 	updated_at = models.DateTimeField(auto_now=True)
 
