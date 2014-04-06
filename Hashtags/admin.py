090d4522b93b248441a42f863cce911e67614541
André Abadesso <andre.abadesso@gmail.com>
2014-04-06 15:29:12 -0300

    Adicionei sugestoes ao painel de admin

--- Hashtags/admin.py
+++ Hashtags/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from Hashtags.models import Hashtag
+from Hashtags.models import Hashtag, Sugestao
+
 
 class HashtagAdmin(admin.ModelAdmin):
-        list_display = ('nome', 'sexo', 'created_at', 'updated_at') 
+    list_display = ('nome', 'sexo', 'created_at', 'updated_at')
 
 admin.site.register(Hashtag, HashtagAdmin)
+admin.site.register(Sugestao)
